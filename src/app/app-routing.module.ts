d0d7b823bd08632073460d177c5b3ae6f55c2a3b
rzkdty <rizki.aditia.ra2@gmail.com>
2023-11-30 12:02:35 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
+import { OtentikasiGuard } from './otentikasi.guard';
 
 const routes: Routes = [
   { path: 'admin', component: AdminComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [OtentikasiGuard],
+  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'mahasiswa', component: MahasiswaComponent },
+  {
+    path: 'mahasiswa',
+    component: MahasiswaComponent,
+    canActivate: [OtentikasiGuard],
+  },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
 
